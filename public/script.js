be1b7bf3d9958f3c54841378bb5124be076a75b3
akki-7781 <akshaychirandas07@gmail.com>
2020-10-30 11:57:53 +0530

    imageExtensionResolve

--- public/script.js
+++ public/script.js
@@ -1,52 +1,65 @@
 var imageData = null;
 
 function hasExtension(fileName, exts) {
-  return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
+  return new RegExp("(" + exts.join("|").replace(/\./g, "\\.") + ")$").test(
+    fileName
+  );
 }
 
-function setdata(event){
+function setdata(event) {
   imageData = event.files[0];
   console.log(event.files[0]);
-  if(hasExtension(imageData.name, [".jpg", ".jpeg", ".bmp", ".gif", ".png"])){
+  if (
+    hasExtension(imageData.name, [
+      ".jpg",
+      ".jpeg",
+      ".bmp",
+      ".gif",
+      ".png",
+      ".JPG",
+      ".JPEG",
+      ".BMP",
+      ".GIF",
+      ".PNG",
+    ])
+  ) {
     var formData = new FormData();
     formData.append("image", imageData);
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       if (xhr.readyState == XMLHttpRequest.DONE) {
         window.location.replace(window.location.href);
       }
-    }
+    };
     xhr.withCredentials = true;
     xhr.open("POST", `${window.location.href}image`);
     xhr.send(formData);
-  }else{
-    document.getElementsByTagName('input')[0].value = "";
+  } else {
+    document.getElementsByTagName("input")[0].value = "";
     imageData = null;
-    alert('only image need to be selected');
+    alert("only image need to be selected");
   }
-  
 }
-
 
 getImages = () => {
   function reqListener() {
     const res = JSON.parse(this.responseText);
     total = res.total;
 
-    const imageContainer = document.getElementsByClassName('row')[0];
-    for(let i=total-1;i>-1;i--){
-      box = document.createElement('div');
-      box.setAttribute('class','col-md-4 col-sm-12');
-      innerbox = document.createElement('div');
-      innerbox.setAttribute('class','box');
-      imgTage = document.createElement('img');
-      imgTage.setAttribute('src',`images/${res.url[i]}`);
-      imgTage.setAttribute('class','preview-image img-fluid');
-      imgTage.setAttribute('id',res.url[i]);
+    const imageContainer = document.getElementsByClassName("row")[0];
+    for (let i = total - 1; i > -1; i--) {
+      box = document.createElement("div");
+      box.setAttribute("class", "col-md-4 col-sm-12");
+      innerbox = document.createElement("div");
+      innerbox.setAttribute("class", "box");
+      imgTage = document.createElement("img");
+      imgTage.setAttribute("src", `images/${res.url[i]}`);
+      imgTage.setAttribute("class", "preview-image img-fluid");
+      imgTage.setAttribute("id", res.url[i]);
 
-      imgLabel = document.createElement('h6');
+      imgLabel = document.createElement("h6");
       imgLabel.innerText = res.url[i];
-      
+
       innerbox.appendChild(imgTage);
       innerbox.appendChild(imgLabel);
       box.appendChild(innerbox);
